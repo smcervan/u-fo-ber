9aa35b2ea1ce2834a9c3f7bddc3193c4e6843c46
smcervan <smcerva8@asu.edu>
2022-12-19 22:16:27 -0700

    Added Player Movement && Camera Movement

---
+++ u-fo-ber/New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    float speed = 20f;
+
+    float horizontalInput;
+    float verticalInput;
+    float rotationSpeed = 20f;
+
+    public GameObject cube;
+    public Transform cubeTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubeTransform = cube.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0f, verticalInput);
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        Debug.Log(horizontalInput); // -1 to 1 when pressed
+        Debug.Log(verticalInput); //-1 to 1 when pressed
+
+        //Rotation needs to be -5 to 5, locally
+        cameraMovement();        
+    }
+
+    void cameraMovement(){     
+        
+        Quaternion currentRotation = cube.transform.rotation;
+        Quaternion wantedRotation;
+
+        float zRot = 0f;
+        float xRot = 0f;
+
+        //Left and Right Movement
+        if(horizontalInput > 0){
+            zRot = -5f;
+        } else if(horizontalInput < 0){
+            zRot = 5f;
+        } else{
+            zRot = 0f;
+        }
+
+        //Forwards and Backwards Movement
+        if(verticalInput > 0){
+            xRot = 10f;
+        } else if(verticalInput < 0){
+            xRot = -10f;
+        } else{
+            xRot = 0f;
+        }
+
+        wantedRotation = Quaternion.Euler(xRot,0f, zRot );
+        cube.transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * rotationSpeed);
+    }
+}
